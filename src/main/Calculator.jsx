907ac43fb96a1a5eb9e29a6f19cac6bf1c6563a6
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-13 09:23:26 -0300

    Update: added display method and a value

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -2,12 +2,15 @@ import React,{Component} from "react";
 import './Calculator.css'
 
 import Button from "../components/Button";
+import Display from "../components/Display";
+
 
 export default class Calculator extends Component {
     render() {
         return (
 
             <div className="calculator">
+                <Display value={100}/>
                 <Button Label='AC'/>
                 <Button Label='/'/>
                 <Button Label='7'/>
