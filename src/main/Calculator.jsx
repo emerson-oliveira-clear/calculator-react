d28c44b1aaf8c08a4aef4741294ee317deb889e8
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-13 09:06:00 -0300

    UPDATE: added buttons

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,17 +1,30 @@
 import React,{Component} from "react";
-
-
 import './Calculator.css'
 
-
+import Button from "../components/Button";
 
 export default class Calculator extends Component {
     render() {
         return (
 
             <div className="calculator">
-
-
+                <Button Label='AC'/>
+                <Button Label='/'/>
+                <Button Label='7'/>
+                <Button Label='8'/>
+                <Button Label='9'/>
+                <Button Label='*'/>
+                <Button Label='4'/>
+                <Button Label='5'/>
+                <Button Label='6'/>
+                <Button Label='-'/>
+                <Button Label='1'/>
+                <Button Label='2'/>
+                <Button Label='3'/>
+                <Button Label='+'/>
+                <Button Label='0'/>
+                <Button Label='.'/>
+                <Button Label='='/>
             </div>
 
         )
