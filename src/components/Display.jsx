778fb8914d3256639acb3fc3945a1e8468c52961
emerson-oliveira-clear <emerson-oliveira-clear@gmail.com>
2023-09-13 10:16:07 -0300

    UPDATE: updated code

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import './Display.css'
 
-export default props =>
+export default props => {
 
-<div className="display">{props.value}</div>
+    return (
+        <div className="display">{props.value}</div>
+
+    )
+}
